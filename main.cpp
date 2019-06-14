009d46758fafe2dddfff0a396a55c8a0ec443a74
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-06-14 14:04:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,72 @@
 #include <ctime>
 #include<vector>
 #include <stdlib.h>
-#include "Human.h"
+#include"HumanFunc.h"
+#include<map>
 
 using namespace std;
 
 int main(void) {
 	setlocale(LC_ALL, "Russian");
 	SortableVector<Human> vec;
+	map<string, HumanData> Humans;
 	if (readHumans("phone_book.txt", vec) == 0) {
-		cout << "не получается окрыть входной файл";
+		cout << "не получается открыть входной файл";
 	}
-	//cout << "Введите новые записи в телефонную книгу" << endl;
-	//cout << "Оставте пустую строку, если ввод записей закончен" << endl;
-	//write_hum("phone_book.txt", vec);
-	string name1,nowaya;
-	getline(cin,name1);
-	nowaya = convert(name1);
-	cout<<findByNameBT(vec,nowaya);
+	write_hum("phone_book.txt", vec);
+	string vib;
+	string name;
+	while (1) {
+		cout << "Выберите способ поиска по телефонной книге: usual, bt, map, или оставьте пустую строку для выхода" << endl;
+		getline(cin, vib);
+		if (vib == "")
+			break;
+		if (vib == "usual") {
+			while (1) {
+				cout << "Введите имя" << endl;
+				getline(cin, name);
+				if (name == "") {
+					break;
+				}
+				else {
+					name = convert(name);
+					cout << findByName(vec, name) << endl;
+				}
+			}
+		}
+		if (vib == "bt") {
+			while (1) {
+				cout << "Введите имя" << endl;
+				getline(cin, name);
+				if (name == "") {
+					break;
+				}
+				else {
+					name = convert(name);
+					cout << findByNameBT(vec, name) << endl;
+				}
+			}
+		}
+		if (vib == "map") {
+			if (read_for_map("phone_book.txt", Humans) == 0) {
+				cout << "не получается окрыть входной файл";
+			}
+			while (1) {
+				cout << "Введите имя" << endl;
+				getline(cin, name);
+				if (name == "") {
+					break;
+				}
+				else {
+					name = convert(name);
+					if (Humans.count(name) == 0)
+						cout << "Заданное имя отсутствует в списке" << endl;
+					else
+						cout << Humans[name] << endl;
+				}
+			}
+		}
+	}
 	system("pause");
 	return 0;
 }
